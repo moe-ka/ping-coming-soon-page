739e98728e8ff8f4bed0989639e56cebb61aa0fd
moe-ka <rizkiizzulhaq14@gmail.com>
2022-12-12 17:27:59 +0700

    update

---
+++ src/script.js
@@ -0,0 +1,28 @@
+// @ts-nocheck
+const btnsubmitEl = document.querySelector(".btn-submit");
+const inputEl = btnsubmitEl.parentElement.children[0].children[0];
+const alertMsg = btnsubmitEl.parentElement.children[0].children[1];
+
+btnsubmitEl.addEventListener("click", function () {
+  if (inputEl.validity.typeMismatch) {
+    inputEl.classList.add("check");
+    alertMsg.textContent = "please provide a valid email address";
+  }
+});
+
+inputEl.addEventListener("input", function () {
+  if (!inputEl.validity.typeMismatch) {
+    alertMsg.innerHTML = "<br>";
+  }
+});
+
+document.addEventListener(
+  "invalid",
+  (function () {
+    return function (e) {
+      e.preventDefault();
+      document.getElementById("name").focus();
+    };
+  })(),
+  true
+);
